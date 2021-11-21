f64456f35de1061e79b3c6fcc36644262bccc229
Leonlav <leonlav77@gmail.com>
2021-11-21 21:32:29 +0100

    react stuff

--- resources/js/components/app.js
+++ resources/js/components/app.js
@@ -1,57 +1,23 @@
-    // resources/assets/js/components/App.js
-
-import React from "react";
+import { render } from "react-dom";
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+// import your route components too
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about" element={<About />}>
-            <About />
-          </Route>
-          <Route path="/users" element={<About />}>
-            <Users />
-          </Route>
-          <Route path="/" element={<About />}>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
