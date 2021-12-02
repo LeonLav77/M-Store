b340d3357e5e305fde6a2df18a616e81511e0a38
MaskoMasko <persic.massimo@gmail.com>
2021-12-02 18:58:32 +0100

    TS TWERKS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,35 @@
-const helloWorld = require("./components/hello-world").helloWorld();
+// const helloWorld = require("./components/hello-world").helloWorld();
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import your route components too
+import { Header } from "./components/Header";
+import { Nesto } from "./components/Nesto";
+import { Miguel } from "./components/Miguel";
+import { Drugi } from "./components/Drugi";
+import { Products } from "./components/Products";
+import { Product } from "./components/Product";
+import { Home } from "./components/Home";
 
-console.log(helloWorld);
+render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:productId" element={<Product />} />
+            <Route path="nesto" element={<Nesto></Nesto>} />
+            <Route path="/miguel" element={<Miguel />}>
+                <Route path="drugi" element={<Drugi />} />
+            </Route>
+            <Route
+                path=""
+                element={
+                    <main style={{ padding: "1rem" }}>
+                        <p>Page not found...</p>
+                    </main>
+                }
+            />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("app")
+);
