5e4dd00b71624fda06248264364fb93130f60611
Leonlav <leonlav77@gmail.com>
2021-12-06 15:17:01 +0100

    email verification

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -33,6 +33,9 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Fortify::verifyEmailView(function () {
+            return redirect('auth.verify-email');
+        });
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
