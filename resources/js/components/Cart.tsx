d1ff1c9783397096d43827dc0c44afe0077988cd
MaskoMasko <persic.massimo@gmail.com>
2022-01-03 18:56:52 +0100

    nap checkout page, bit ce responsive kad dodan podatke

--- resources/js/components/Cart.tsx
+++ resources/js/components/Cart.tsx
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Cart = () => {
-    return <div>Cart</div>;
+    const [cart, setCart] = useState(Array(3).fill("item"));
+    return (
+        <div style={{ width: "70%", marginLeft: 20 }}>
+            <table cellPadding={20}>
+                <thead>
+                    <tr
+                        style={{
+                            padding: 20,
+                            textAlign: "center",
+                            justifyContent: "space-around",
+                            fontSize: 24,
+                        }}
+                    >
+                        <th style={{ minWidth: 200, width: 250 }}>Image</th>
+                        <th style={{ minWidth: 200, width: 250 }}>Name</th>
+                        <th style={{ minWidth: 300, width: 400 }}>Quantity</th>
+                        <th style={{ minWidth: 200, width: 250 }}>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map((cartItem, id) => {
+                        return (
+                            <tr key={id}>
+                                <td>
+                                    <img
+                                        src="cartItemImg"
+                                        alt=""
+                                        height={150}
+                                        width={150}
+                                    />
+                                </td>
+                                <td style={{ display: "flex" }}>Item.Name</td>
+                                <td
+                                    style={{
+                                        position: "relative",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    <div
+                                        style={{
+                                            position: "absolute",
+                                            top: "15%",
+                                            left: "47%",
+                                            transform: "rotateZ(-90deg)",
+                                            fontSize: 36,
+                                        }}
+                                    >
+                                        &gt;
+                                    </div>
+                                    Item.Number
+                                    <div
+                                        style={{
+                                            position: "absolute",
+                                            bottom: "15%",
+                                            left: "47%",
+                                            transform: "rotateZ(90deg)",
+                                            fontSize: 36,
+                                        }}
+                                    >
+                                        &gt;
+                                    </div>
+                                </td>
+                                <td style={{ display: "flex" }}>Item.Price</td>
+                            </tr>
+                        );
+                    })}
+                    <tr></tr>
+                </tbody>
+            </table>
+        </div>
+    );
 };
--- resources/js/pages/CartPage.tsx
+++ resources/js/pages/CartPage.tsx
@@ -1,12 +1,84 @@
 import React from "react";
 import { Cart } from "../components/Cart";
 import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
 
 export const CartPage = () => {
     return (
         <div>
-            <Cart />
-            <Link to="/checkout">Go To Checkkout</Link>
+            <Navbar />
+            <div
+                style={{ display: "flex", flexWrap: "wrap" }}
+                className="cart_container"
+            >
+                <Cart />
+                <div
+                    style={{
+                        width: "30%",
+                        backgroundColor: "whitesmoke",
+                        borderRadius: 15,
+                        margin: 40,
+                        boxShadow: "4px 4px 4px rgba(0, 0, 0, 0.2)",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        height: "fit-content",
+                        paddingBlock: 20,
+                        gap: 15,
+                    }}
+                    className="checkout_container"
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "90%",
+                            marginInline: "auto",
+                        }}
+                    >
+                        <h4>Item(1):</h4>
+                        <h4> 20lkn</h4>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "90%",
+                            marginInline: "auto",
+                        }}
+                    >
+                        <h4>Shipping:</h4>
+                        <h4> 3kn </h4>
+                    </div>
+                    <hr style={{ width: "90%", backgroundColor: "#333" }} />
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "90%",
+                            marginInline: "auto",
+                        }}
+                    >
+                        <h2>Total: </h2>
+                        <h2>352kn</h2>
+                    </div>
+                    <button
+                        style={{
+                            width: "80%",
+                            backgroundColor: "#333",
+                            height: 50,
+                            marginInline: "auto",
+                        }}
+                    >
+                        <Link
+                            to="/checkout"
+                            style={{ color: "white", listStyle: "none" }}
+                        >
+                            Go To Checkkout
+                        </Link>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
