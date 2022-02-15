60f24926e4bda32e6585f5a13051f345a13eb9a2
MaskoMasko <persic.massimo@gmail.com>
2022-02-15 21:59:16 +0100

    pass confiramtion dela(ni povezano)

--- resources/js/components/ConfirmPassword.tsx
+++ resources/js/components/ConfirmPassword.tsx
@@ -1,61 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import $ from "jquery";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const ConfirmPassword = () => {
     const navigate = useNavigate();
-    function confirmPassword() {
-        $.ajax({
-            method: "POST",
-            url: "/auth/user/confirm-password",
-            dataType: "json",
-            contentType: "application/x-www-form-urlencoded",
-            data: {
-                password: "password",
-                password_confirmation: "password",
-            },
-            success: (result) => {
-                console.log(result);
-            },
-            error: (error) => {
-                console.log(error);
-            },
-        });
+    const [falsePassword, setFalsePassword] = useState(false);
+    function confirmPassword(): any {
+        try {
+            let response = axios({
+                method: "post",
+                url: "/auth/user/confirm-password",
+                data: {
+                    password: "password",
+                    password_confirmation: "password",
+                },
+            })
+                .then((result) => {
+                    setFalsePassword(false);
+                    return result;
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setFalsePassword(true);
+                });
+            return response;
+        } catch (err) {
+            return err;
+        }
     }
     function enableTFA() {
-        $.ajax({
+        axios({
             method: "POST",
             url: "/auth/user/two-factor-authentication",
-            dataType: "json",
-            contentType: "application/x-www-form-urlencoded",
-            success: (result) => {
+        })
+            .then((result) => {
                 console.log(result);
-            },
-            error: (error) => {
-                console.log(error.responseJSON.message);
-                console.log(error.status);
-            },
-        });
+            })
+            .catch((err) => {
+                console.log(err.responseJSON.message);
+                console.log(err.status);
+            });
     }
     return (
-        <div>
-            <h1>Confirm Password</h1>
-            <input type="text" placeholder="Password"></input>
-            <button
-                onClick={() => {
-                    confirmPassword();
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                backgroundColor: "whitesmoke",
+            }}
+        >
+            <div
+                style={{
+                    width: 400,
+                    margin: 50,
+                    height: 250,
+                    backgroundColor: "#ebebeb",
+                    borderRadius: 15,
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 15,
+                    textAlign: "center",
+                    justifyContent: "center",
                 }}
             >
-                Confirm
-            </button>
-            <button
-                onClick={() => {
-                    enableTFA();
-                    navigate("/user_profile");
-                }}
-            >
-                enable tafa
-            </button>
+                <h2 style={{ margin: 0 }}>Confirm Password</h2>
+                <input
+                    style={{ width: "70%", marginInline: "auto" }}
+                    type="text"
+                    placeholder="Password"
+                ></input>
+                <button
+                    style={{ width: "70%", marginInline: "auto" }}
+                    onClick={() => {
+                        const tryPasswordConfirmation = async () => {
+                            const results = await confirmPassword();
+                            if (results.status == 201) {
+                                await enableTFA();
+                                navigate("/user_profile");
+                            } else {
+                                throw new Error("Wrong passworddd!");
+                            }
+                        };
+                        tryPasswordConfirmation();
+                    }}
+                >
+                    Confirm
+                </button>
+                {/* <button
+                    onClick={() => {
+                        enableTFA();
+                    }}
+                >
+                    enable tafa
+                </button> */}
+            </div>
         </div>
     );
 };
--- resources/js/hooks/useAuth.tsx
+++ resources/js/hooks/useAuth.tsx
@@ -27,7 +27,7 @@ export const AuthUserProvider = ({ children }) => {
                 method: "post",
                 url: "auth/login",
                 data: {
-                    email: "leonlav77@gmail.com",
+                    email: "massimopersic1@gmail.com",
                     password: "password",
                 },
                 headers: {
